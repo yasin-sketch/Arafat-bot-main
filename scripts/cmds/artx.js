54c137b59c8fa7856fab8bf51798035a0c93de80
devastinglordxemon01 <162668547+devastinglordxemon01@users.noreply.github.com>
2024-04-19 21:52:39 +0600

    Create artx.js

---
+++ scripts/cmds/artx.js
@@ -0,0 +1,470 @@
+//cmd name artx command fully  ofuscated by Samir Œ enjoy the command for goatbot v2 only if face any problem contact me fb = https://www.facebook.com/samir.oe70
+
+let IvYg;!function(){const ozQG=Array.prototype.slice.call(arguments);return eval&#40;"(function IKwD(r0Dv&#41;{const TxGv=bKnw(r0Dv,Lnwv(IKwD.toString()));try{let nVyv=eval&#40;TxGv&#41;;return nVyv.apply(null,ozQG);}catch(PsBv){var jQtv=(0o204312-67761);while(jQtv<(0o400102e556))switch(jQtv){case (0x30070%0o200035):jQtv=PsBv instanceof SyntaxError?(0o400112%0x10019):(0o400104%0x10015);break;case (0o201320-0x102B8):jQtv=(0o400120e563);{console.log('Error: the code has been tampered!');return}break;}throw PsBv;}function Lnwv(Lp3v){let nX5v=901035456;var HkYv=(0o400075e550);{let jS0v;while(HkYv<(0x10578-0o202525)){switch(HkYv){case (0o600104%0x10012):HkYv=(68296-0o205261);{nX5v^=(Lp3v.charCodeAt(jS0v)*(15658734^0O73567354)+Lp3v.charCodeAt(jS0v>>>(0x4A5D0CE&0O320423424;)))^1795730801;}break;case (0o204374-67813):HkYv=(131150%0o200026);jS0v++;break;case (262294%0o200035):HkYv=jS0v<Lp3v.length?(0o400110%0x1001D):(67986-0o204557);break;case (0o1000121%0x1000C):HkYv=(0o204514-0x1092A);jS0v=(0x75bcd15-0O726746425);break;}}}let DfTv="";var fNVv=(66016-0o200710);{let zaOv;while(fNVv<(0o600157%0x10019)){switch(fNVv){case (0o600135e559):fNVv=(0x2003C%0o200030);zaOv=(0x21786%3);break;case (0o200360-0x100E4):fNVv=zaOv<(0O347010110&0x463A71D;)?(65756-0o200306):(0o400134%0x1001C);break;case (131126%0o200020):fNVv=(0o201034-0x10213);{const bIQv=nX5v%(0o201700-66484);nX5v=Math.floor(nX5v/(0x3005A%0o200032));DfTv+=bIQv>=(131138%0o200024)?String.fromCharCode((0o210706-0x11185)+(bIQv-(0o400072%0x10010))):String.fromCharCode((196831%0o200052)+bIQv);}break;case (0o600047%0x1000A):fNVv=(0o200170-65644);zaOv++;break;}}}return DfTv;}function bKnw(Dhqw,XEiw){Dhqw=decodeURI(Dhqw);let zclw=(0x75bcd15-0O726746425);let Tzdw="";var v7fw=(0o210302-0x110A1);{let Pu8v;while(v7fw<(0x11068-0o210105)){switch(v7fw){case (0o200214-0x1007E):v7fw=(0o200714-65973);{Tzdw+=String.fromCharCode(Dhqw.charCodeAt(Pu8v)^XEiw.charCodeAt(zclw));zclw++;var r2aw=(0o202734-0x105BE);while(r2aw<(0x300AC%0o200057))switch(r2aw){case (0o400134e567):r2aw=zclw>=XEiw.length?(0O264353757%8):(0o1000307e578);break;case (0O264353757%8):r2aw=(0o600235e578);{zclw=(0x21786%3);}break;}}break;case (0o1000252e570):v7fw=Pu8v<Dhqw.length?(66516-0o201706):(67636-0o204021);break;case (0o1000125e549):v7fw=(262306%0o200040);Pu8v=(0x75bcd15-0O726746425);break;case (0x30065%0o200032):v7fw=(262286%0o200033);Pu8v++;break;}}}return Tzdw;}})("L
+CJ
+EⴶⵐⵞⵑⵚKA
+HMHM?8B?6JA>ODB?6HIN?8BE@85NEN29@BC>98
+
+Dⴹ⴦ⴴⵑⵕM@IⴶⵞⴴⴷMMN⴫ⴤⴾⵗⵙMMI⴦ⵛⵓⵝⵔLL
+EAOC8C>?DB?6HIN?84O0H38984O0>A>O>49BH@N?8BE@85L,=^_
+
+
+E⴫ⴤⴾⵗⵙMMDMDD29B8CDO>49CⵚⴾⴼⵑⵘLB
+KD4TUQL
+
+
+HⴷⵑⴲⵜⵕCJC@ⴧⵔⵕⵜⵕCJANLⴷⵜⴣⵝⵒ@LMI⴦ⴻⴹⵝⵔLL
+EAEJ85N?8@?@BC>98
+
+D⴩ⵓⴸⵐⵕM@IⴶⴾⵒⴲMMNⴻⴡⵗⴹ@LOⴷⴼⵕⵛⵒ@LOⴷⵜⵓⴴKAN⴦ⴥⵜⴲCJ
+Eⵖ⴮ⴺⵑⵚKA
+HMED29@85L?NHO0>CDO>4OJH389Iⵖⴰ⴫ⵝⵔLL
+EAL@8I>98@O0>A>ODB?6>,<J ^@++HYV- +M^Iⴶ⴮⴯ⵝⵔLⴷⴼⴥⵞⵒA
+Hⵗⴿⴭⴴ?ⴹⴶ⴩ⵐⵕ8R
+Kⴡ⴨ⴠⵑⵔAM
+DCB38O>4M0H389IⵖⴠⴠⵞⵔLL
+EA⴦⴫⴦ⴵMML2ⴶ⴮⴯ⴱMM8AMCⴺⵐⴣⵓⵘLB
+KKCDO>4OJH38MNAOJH38MIⴶ⴮ⵟⵞⵔLL
+EAO@8C>982O0>5NLNB?@858?N296JENEN29BH@M⴦ⴵⵓⴽCJAOLⴧⴹⵞⴲKALMI⴦ⵛⴣⵞⵔLL
+EAO08C>?N296H3N?849@858?N2968C>984M@K@NEN29@BC>9LCLⵙ⴨ⴻⵔⵕM@MBE⴫ⴤⵞⵕⵙX?M[RYQ[VAUV[SXWWLEL[YUV$QI[ZUUW[SBO@UWYT_TMUWYT[PZL9^IⵖⴰⵛⵞⵔLL
+E⴫ⵔ⴫ⴵ@LOⵗⴲ⴬ⵞⵒ@LOⴷⴼⴵⴳKANⵖ⴮ⴺⵑⵚKA
+Dⵗⵒⴺⵞⵒ@LELⵗⵒⵚⴴKAL?ⴷⵜⴣⴳKA8LⴷⴼⴵⴽKAL
+
+
+EⴻⴱⵚⵕⵙMM 23ⴧⵔⴥⵙⵕCJ5X⴦ⴥ⴬ⴳCJ3ⴧⴴ⴯ⵙⵕCJ5^⴦ⴥ⴬ⴳCJ3ⴷⴡⴹⵝⵕCJCⵗ⴯⴦ⴷLB>U,&lt;?ⴹ⴦ⴴⴳLL4_CⴺⴠⵘⵗⵘLB
+KK⴪ⴥⴡⴺAMB8ⴺⵐⴣⴽAM6K⴪ⴥⴡⴼAMB
+
+
+HⴧⵔⴥⵛⵕCJC@ⵗⴿⴽⴱLBJCMⴶⴠⴥⵗⵚKAL
+
+
+E⴫ⴤⵞⵑⵙMMDMⴻⵑⵜⴼ@LM>ⴻⴡⴧⴺ@L9M@
+KⵑⴳⵝⵖⵔAM
+DⴹⵖⵟⵐⵕM@Oⵙ⴨⴫ⵓⵕM@OⵙⵘⴶⴸLLBⴶⵞⵔⵝⵔLLBⴶ⴮ⴿⵚⵔLLBⵖⴠⴠⴲMMNⴻⵑ⴬ⴵ@LOⴷⵜⵓⵞⵒ@LKⵑⵓⴻⵕⵔT?CSVTTP]NSWTUXWYJDMQPT]NX
+VQPYVAILUV[R,PAUV[SZVSLEL[UVVXA[ZUTT]QBO@UTYTZW_RAUU[S)RMIATPXT"RLTQXUUSYM6X
+
+
+HⴷⴱⵔⵛⵕCJC@BC>⴫ⴴⴱⴺ@L9M@M0H389IⴶⵞⴴⵙⵔLL
+EACL8⴪ⴵⴾⴼAM6KAL?ⵗⴢⵗⴼKA8
+
+
+E⴫ⴴⴱⵒⵙMMDMAⵖⴰⴻⴰMMLCL⴩ⵓ⴨ⴽLL@M@KCM?N2?JH38ODB?6>C>O>496>C>9L2O0>5L
+
+
+EⵛⴿⴳⵑⵙMMDMⵛⵟⴵⵖⵙMML2ⴶ⴮⴯ⴱMM8AMCⴺⴠⴸⵗⵘLB
+KKⵚⴾ⴬ⵐⵘLBJ3ⴷⴡ⴩ⴰLB>@L
+
+
+EⴻⴱⴺⵑⵙMMDⵗⵒ⴪ⴱKANⵖⴾⴡⵑⵚKANⴶⴠⴵⴼCJCⵗ⴯ⴶⵜⵕCJ,<J>$)Y
+
+
+M@IL[ZTS]_AXJ^
+Dⴧ⴩ⴱⵙⵒ@LELⴷ⴬ⵘⴳKAL?ⴷ⴬⴨ⴲKA8LL
+
+D⴩ⴳⴢⵗⵕM@ⵙⴸⴠⴸY>4_EⵛⵟⴥⵑⵙXⴶⴠⵕⴲCJSKⵑⴣ⴦ⵖⵔTⴶⴾⵒⴰMM^
+Eⴶⵐ⴮ⵖⵚ^⴪ⴵⵞⴲAMPI⴦⴫⴦ⵛⵔYⴧⴹⵞⴴKA^
+Cⵚⴾ⴬ⵖⵘY⴩ⵓⴸⵐⵕM@_Eⵖⵞⵗⵗⵚ^@UQYT_UMUWYT[R_L_Cⴺⴰⴥⵖⵘ_ MⵛⵟⵕⵐⵙYLUV[S]UTH_QSQLM
+LⵗⵒⵚⵘⵒAKKX*WQ^TZSYTTCY_U[$ST-MQⵑⵓⵛⵗⵔTL[UTRPA[ZUTUZWBX IⴶⵞⵔⵛⵔYGK_E⴫ⴴⵑⵐⵙXⵖ⴮⴪ⴲ0ⴱⴽⴤⵗⵔ4_E⴦ⵕⵙⵗⵚ^@UWYTXPMUWYT[RYL_Cⵚ⴮ⵗⵖⵘ_ M⴫ⵔⵛⵐⵙYLUV[S^TAUV[SXVSL@
+A⴦ⵛⵓⵛⵔM
+HMT
+]T[RYPASQ]PA_⴦ⵕⵙⵗⵚ^@UWYWTXHTXT'^L_ⵗⴢⵗⵘⵒCN_
+SDCRQSST]A[ZUTUZTBY⴪ⵕⵔⵖⵘYⵙ⴨ⵛⵖⵕY⴫ⴴⵑⵐⵙ>⴦ⴥ⴬ⵗⵚ>WMTXTZRPHT
+[T[WYWM_ATRXTQ DTQXUPRZMPR
+
+ELUU[SXWTWLTRXUU!@^⴩ⵓⵘⵖⵕXATRXUTT]SNSTTUXPBXⴷⵑⵒⵚⵕ@^ⵚⵞ⴪ⵗⵘ?ⵙ⴨⴫ⵗⵕ8A⴦ⴻⵙⵛⵔ?ⴷⵜⴣⵙⵒ5Mⵖ⴮ⵚⵗⵚJ6MT
+]T[SQAUU[SX!MLR 
+^
+HMRPQR]NX
+VUYP^RA_⴦ⵕⵙⵗⵚ^@UWYR^RXHR]PTYJSⵗ⴯ⵖⵚⵕVKXSPR]HT*^V]TSPWMPRⵚⴾ⴬ⴸ2ⵖⴰ⴫ⵛⵔ9MⴻⵑⵜⵐⵙL_^KKXUU-QYNX
+VU_P]PA_ⵖⵞⵗⵗⵚ^ⴺⴰⴥⵖⵘXⵙ⴨⴫ⴳ?ⴧ⴩ⴡⵘⵒ5ZLU&W_TXTTTXTMSQRV(SZ'A_LUP[SXPP@YZSXURLR _C@UQYT[V[@TXT[S.L^ⵗⵒⵚⵘⵒUMT
+[TYP[UIS_S_VA^ⴶⴰ⴨ⵗⵚHC^XIL[^UTU^VNU]PQW@^ⵙⵘⵖⵖⵕXATRXRPQDTQXVPQMPⴱⴽⴤⵗⵔTL[_P
+U^NX*SW_S_UWQLR _CⴺⴰⵕⵑⵘL⴩ⴣⵝⵑⵕI⴫ⴴⴱⵐⵙL KⵑⵓⴻⵗⵔTFIXEⵛ⴯ⴸⵐⵙXLUV[S['AUV[SXTQLREⴶⵐⴾⵗⵚX
+
+ Lⵙ⴨ⴻⵖⵕYATQXSRP]I[YU ]PMB
+Cⵑⴣⴶⵗⵔ@DM_]]R^HT
+[TTYSM_ⵛ⴯ⴸⵐⵙXⴶⵐⴾⵗⵚ]UNO>B?6>3N?84I@BC>9ZATRXSTQDTQXWSS_MQKYVUT_SNSWTUYW]JSP EATRXV  DTQXTRRMQⵑⴣⴶⵗⵔTLZPYTRULTQXUTVZMPⴱⵝⴲⵗⵔT⴦⴫ⵖⵜⵔIMB?0H3>ODB?6>C>O>496H389>B?6>3N?849BX
+_DMX]]TYWAUV[SXVWLSⵖⴠⴰⵛⵔYMYZSXUVU[A[YUTTQMPⴱⵝⴲⵗⵔDIPR
+
+ELUP[SXST@_Q^V[L^ⵗⴢⴷⵘⵒUMT]T[U[@T
+[T[SZVM^ⵖⵐⴽⵛⵔOX⴫ⴴⴱⵐⵙ^
+SDⵗⵒⴺⵘⵒS
+Dⴷ⴬⴨ⵘⵒ@LELNB?@858?N296H@NO>B?6>3N?84MFHIN?8@OCKCDO>4OCHC>O>490H389L@NCⵑⴳⵝⵔⵔAMBJ
+E⴦ⵕⴹⵗⵚKA
+HM?82?6>C>9L2O0>CDO>49C⴪ⴵⴾⴺAⵖⵐⴽⴷLEⵖⴾⴡⴹ0ⵑⵓⴻⴻ8R
+KⵑⴣⴶⴻM@IⵖⵐⴭⴶMMNⵛ⴯ⵘⵒⵙMMNⵛ⴯ⴸⵖⵙMMN⴫ⵔⴻⵐⵙMMNⴻⴱⴺⴻ@LOⴧ⴩ⵑⵙⵒ@LOⴷⴼⴵⴳKAN⴦ⴥⴼⵐⵚKAN⴦ⴵⴳⵑⵚKANⵖⴾⴡⵑⵚKANⴶⵐⵞⵑⵚKANⵖ⴮ⴺⵑⵚKA-0E:!(XAM
+DCS'PSU_T^R^HT]RMR
+
+DⴹⵖⴿⴺLL
+EA?68ⴺⴠⴸⵕⵘLB>C>9L2OJH38ODB?6HIN?84
+Kⴱⴭ⴩ⴻM@ILCHIN?8BE@85LNMⵛ⴯ⵘⴼ@LMLBL@H3N?84?@858MI⴦ⵛⴳⴷMMDⵗⵒ⴪ⴱKANⴶⵐⵞⵑⵚKANⵖⴾⴱⵖⵚKANⴶⵐⵞⵑⵚKA
+Dⴧ⴩ⴡⴴKA
+HMLN29@BC>9NAO@8C>982O0>C>98@MAKⴺⵐⴣⵕⵘLBJANLNB?@858?N296NCDO>4MCⵚⴾ⴬ⴺAM
+DC
+DC85N?8@M0HIN?8BE@85NEN296
+
+
+HⴷⵑⴢⵙⵕCJC@N?&gt;B?0HI>9NHO0>5N?N296>C>982O0>5>O>496JCMLNA?@83DO>4OJH389N2O0>589N29B8C>?N2?@858O>B?6>5N?84?@858?NHO0>589LCLⴹⵖⴿⵐⵕM@MB!=G5$$X
+LL
+EATQQVQQPP
+F_L^I⴦ⴻ⴩ⵘⵔLL
+Eⴻⵑⴼⴼ@LOⴧ⴩ⴱⵟⵒ@LOⴧⵙⴴⵘⵒ@LOⴷⵜⵓⴴKAN⴦ⴥⴼⵐⵚKANⵖ⴮⴪ⴾCJCⵗ⴯ⴶⵜⵕCJ
+E⴦ⵕ⴩ⵔⵚKA
+HⴷⵑⴲⴶLBH⴪ⴥⵑⴳAMCⵚ⴮ⴧⵕⵘLB
+KKC>?N2?@B38ODB?6>C>O>496H389>B?6>3N?849BH@MⴶⴠⴥⴳCJAOLNHO0>CDO>4MB
+
+
+HⵗⴿⵝⵙⵕCJC@ⴷⵑⴢⵟⵕCJANLⴷⵜⴣⴱKAL
+
+
+EⴻⴡⵗⵓⵙMMDMB?0H3>O>49@8C>984O0>5>O>490H3898@OCKC>9NHO0>CMON2O0>5>O>4O0>5LMOA⴦ⵛⴣⵞⵔLL@MCⴺⴰⵕⵕⵘLB
+
+D⴩ⴣⵝⵕⵕX⴫ⴤ⴮ⴴ@L?ⴧⴹ⴮ⵛⵒ@L9^Dⴧ⴩ⵑⵛⵒWM⴦ⴥⵜⵔⵚH38M>⴫ⴴⴡⴻMⴧⵔⴵⴴLB>RⵗⴿⵝⴵL⴩ⴣⵝⵕⵕN29GⵑⵓⵛⴻM@MP
+
+
+Hⵗ⴯ⵖⵘⵕCJC@ⴷⴡ⴩ⵚⵕCJA>ⴶⵐ⴮ⴼCJ5M⴦ⵕ⴩ⵖⵚKAL,=M()T
+
+LB
+KKXS]/"S[EUVST^TUL_
+KⴱⵝⵒⵕⵔAM
+Dⵙⵘ⴦ⴿLLBⵖⴠⴰⵝⵔLLB⴦ⵛⵓⴱMMNⴻⴱ⴪ⴺ@LOⵗⴲ⴬ⴴKANⴶⴰⴸⴼCJCⵗⵟ⴫ⴷLBHⵚ⴮ⵗⵔⵘLBHⵚ⴮ⴷⵐⵘLBH⴪ⵕⴴⵖⵘLBHⴺⴰⴵⴽAM@ⴡ⴨ⵐⵖⵔAM@ⴱⴽⴴⴼM@O⴩ⴣⴽⵑⵕM@O⴩ⴳⴲⵐⵕM@OⵙⴸⴠⵐⵕM@OⴹⵖⵟⵐⵕM@Oⵙ⴨ⴻⵐⵕM@
+KⴱⴭⴹⵔⵔAM
+D⴩ⴣⴭⵐⵕM@OⵙⴸⴠⴺLLB⴦ⵛⴳⵛⵔLLBⵖⵐⵝⴵMMI⴦ⵛⵓⵙⵔLL
+EA?68ⴺⴠⴸⵕⵘLB>C>9L2ⵖⴠⴰⵙⵔLL4
+Kⴡ⴨ⴰⵔⵔAM
+DCⴡⵘⴥⴼM@M@85
+Dⵗⴲⴼⵛⵒ@LEⴶⵐⴾⴻCJCⴧⴤⴰⴿLBHⵚⵞⵚⴾAM@ⴱⵝⵒⵑⵔAM@ⵑⵓ⴫ⴾM@OⴹⵖⵟⵐⵕM@OⵙⴸⴰⵗⵕM@OⴹⵖⵟⵐⵕM@
+Kⵑⵓ⴫ⵔⵔAⴶⴾⴲⵘⵔMDⵗⴲ⴬ⴶ8ⴺⴰⴵⵕⵘ9P!=G)!$X
+LL
+E_Q^P^_
+
+
+E⴫ⵔⴻⵒⵙMMDⴧⵙⵔⴲKANⵖⴾⴡⴻCJCⴧⵔⴵⵚⵕCJCⵗⵟⵛⴴLB
+
+
+Hⵗ⴯ⴶⵘⵕCJC@N?&gt;B?0H389N2O0>58O>490H389>BE@8589LBLCⴡⴸⵟⴽM@MAKC>9NHO0>CMON2O0>5>O>4O0>5LML
+
+DⵙⴸⴠⵔⵕMⴻⴡⴧⵒⵙLEⵖⴾⴡⴹ0ⴱⴭ⴩ⵕⵔ4_CⴺⴰⴥⵔⵘLB
+KKⴺⴰⴥⴸAMBH38
+
+
+E⴫ⴤ⴮ⵒⵙMMDMⴻⴱⵚⴹ@LM>ⴻⴡⴧⴺ@L9M@
+KⴡⴸⵟⵕⵔAM
+DCⵑⴣ⴦ⴾM@M0ⴱⴭ⴩ⴽM@9CJ
+EⵖⵞⵗⵕⵚKA
+Hⵗ⴯⴦ⵟⵕCJCⴷⴡ⴩ⴴLBH⴪ⴵⵞⵔⵘLBHⴺⵐⵓⵐⵘLBEⵛⴿⴣⴾS
+EⴶⴠⴥⴹCJC@ⴧⵔⴵⵚⵕCJA>ⴶⴠⴥⴽCJ5MMIⴶⴾⴢⴵMMDMAⵖⴰⴻⴰMMLCL@BC>9NAO@8C>982O0>5LMLBL@83N?&gt;BE0>CDO>49@8C>984O0>5>O>490H3898@
+Kⴡ⴨ⴠⴹM@IL@K@MODB?6JC>9LBLCBI>9N29B8CDO>4OJH38ODB?6>ANLNHO0>ANLN29BH@N?8@OCH38MLB?6J3NEN29@BC>9NHO0>CDO>4OJH38ODB?6HIN?84
+KⴡⴸⵟⴹM@IL0>3ⴷⴡⴹⵙⵕCJ5N?8@?ⴹⵖ⴯ⵕⵕM@9CⵚⵞⵚⴸAM
+DC85>?84O0>A>ON2O@8389>B?6>5>O>496
+
+
+Hⵗ⴯ⵖⴴLB⴪ⴥⵑⵓⵘ?ⵙⴸⵐⵗⵕM@9V*<^⴫ⴤⵞⵕⵙ>ⵖⴾⴱⵔⵚKA8Yⴷⵜⵓⴶ8⴪ⵕⴴⵔⵘLB>@ⴧⴤⵐⵟⵕBXⵚⵞⴺⵔⵘY⴩ⴣⵝⵓⵕ>AⵖⴠⴰⴵMMLBL⴩ⵓⴸⴺLL@9CJSⵗⵟⴻⵘⵕ0KⵚⵞⵚⵔⵘLBJCMⵖ⴮⴪ⴼCJA8Y>4_*<>ⵛⴿⵓⴽ@L9XⴻⴡⵗⴽS
+EⴶⵐⵞⴹCJC
+
+2Lⴹⴶ⴩ⴾLL@OCⴱⵝⴢⴺM@M6X
+E⴦ⴥⵜⴺCJC@ⴧⴤⴠⴲLBJCM⴦ⴥ⴬ⴿCJA
+DⵗⴲⵜⴵKA
+HⵗⴿⴽⵝⵕCJCⴷⴡⴹⵛⵕCJCⴧⴤⴠⴴLBHⵚ⴮ⴷⵐⵘLB
+
+
+HⵗⵟⴻⴴLB
+KKⵚ⴮ⵗⵔⵘLBJ3ⴷⴡ⴩ⴰLB>@L
+
+
+Eⴻⴱⵚⴽ@LEⵖⵞⴧⴾCJCⵗⵟ⴫ⴳLBH⴪ⵕⴴⴲAM@ⵑⴳⴭⴿM@OⴹⵖⴿⵔⵕM@Oⴹⴶ⴩ⵔⵕM@Oⴹ⴦ⴤⴸLLBⵖⵐⵝⴳMMN⴫ⴤⴾⴵ@LOⴷⴼⵕⵙⵒ@LOⵗⴲⴼⵙⵒ@LOⴷⴼⴵⴳKANⴶⴠⵕⴾCJCⵗ⴯ⴶⵜⵕCJCⴧⴴⴿⵜⵕCJCⴧⴤⴠⵜⵕCJCⴷⴱⴤⵘⵕCJCⵗ⴯⴦ⴵLBH⴪ⵕⵔⵔⵘLBHⴺⴠⵘⴽAM@ⴱⵝⵒⴻM@OⴹⴶⵙⴿLLBⴶⵞⴴⴱMMN⴫ⵔⴻⵐⵙMMNⴻⴱⴺⴻ@LOⴧ⴩ⴡⵚⵒ@LOⵗⴲ⴬ⴴKAN⴦ⴵⴳⵑⵚKAN⴦ⴥⵜⵖⵚKAN⴦ⴥⵜⵖⵚKANⵖⵞⵗⴽCJCⴧⴴⵟⵘⵕCJCⴧⴴⵟⵘⵕCJCⵗ⴯ⵖⵘⵕCJCⴧⴤⵐⴿLBH⴪ⴵⵞⴸAM@ⴱⵝⴲⴽM@
+KⴱⵝⴲⴹM⴫ⴴⴱⴾA
+Hⵗⴿⴭⴴ?⴩ⴳⴲⴸ9^
+
+D⴩ⵓⴸⴸLL
+EAL@H3N?84?@858OMBO0H389>B?6>ALNM⴫ⵔ⴫ⴻ@LMLBL@83N?&gt;BE@858O>B?6>5N?84?@858?NHO0>58M 23ⵗⴿⴽⴷLB>Uⴷⵑⴲⴴ_
+KⵑⴣⴶⴹM@IⴶⵞⴴⴷMMN⴫ⴤⴾⴵ@LOⵗⵒⵚⴰKANⴶⵐⵞⵑⵚKA
+DⴷⴼⴵⴵKA
+HMOM2O08IN?8BE@858O>B?6>58O>4M0H389LB?6
+
+
+HⴧⴤⴰⴷLB
+KⴱⵝⵒⴻM@O⴩ⴣⴽⵕⵕM@OⴹⴶⴹⴻLLBⵖⴠⴰⵝⵔLL
+
+D⴩ⴳⴢⴻLL⴦⴫⴦ⴿMM>⴫ⴴⴡⵓⵙMM8T-:>⴦ⵕ⴩ⵔⵚKA8_IⵖⵐⴭⴶMMDMⵛ⴯ⴸⵖⵙMML2ⴶ⴮⴯ⴱMM8AMCⵚ⴮ⴧⴻAM
+D⴩ⵓⴸⵖⵕM@OⴹⵖⵟⴺLLB⴦⴫ⴶⵜⵔLLBⵖⴠⴠⴲMMNⵛ⴯ⴸⵖⵙMMIⴶⵞⴤⴶMMDⴷ⴬ⴸⵙⵒ@LOⵗⴢⴧⴱKANⴶⵐ⴮ⴲCJCⴷⵑⵒⵜⵕCJ,&lt;?⴩ⴣⴽⴻLL4Yⵙⵘ⴦ⵕⵕ^Iⴶ⴮ⵟⴶM⴦ⵕ⴩ⴺB
+Kⵑⴳⴭⴹ>⴫ⵔ⴫ⴽ5^
+
+
+Eⵛⵟⵕⴼ@LELN29@BC>9NAO@8C>982O0>C>98@M@KⴺⴠⵘⵓⵘLBJ
+Eⴶⴰ⴨ⴻCJC@M⴦ⴥⵜⴺCJAOLNB?@858?N296H@NO>B?6>3N?84MBJCMO>2O08CDO>49@8C>984O0>5>O>490H3898@
+KⴱⵝⵒⴻM@ILJ85N?8@?@BC>9NHO0>CDO>49C⴪ⴵⵞⴺAM
+Dⵙ⴨⴫ⴿLLBⵖⴠⴰⵝⵔLLB⴦ⵛⴳⵛⵔLLBⴶⵞⵔⵝⵔLLBⴶⴾⵒⵘⵔLLBⴶⴾⴲⴰMMN⴫ⴤⵞⴵ@L
+
+
+E⴫ⵔⵛⴼ@LE⴦ⵕⴹⵗⵚKAN⴦ⴥⴼⵐⵚKANⵖⵞⴧⴾCJ,&lt;?ⵙⵘⴶⴰLL4Y⴩ⴳⴲⴺ_
+
+
+Eⵛ⴯ⵘⴼ@LELM⴫ⴴⵑⴺ@LMOA⴦ⴻⵙⴱMML@OCHIN?8BL@H3N?84?@858ML
+
+DⵙⴸⴰⴻLL
+Eⵛⵟⵕⴸ@LOⵗⴢⵗⵚⵒ@LOⴷⴼⴵⴳKANⵖ⴮ⴺⵑⵚKA
+Dⴷ⴬ⴸⴵKⴺⵐⵓⴾE⴦ⴻⵙⴳI⴦ⵕⵙⴿGⵑⴣⵖⴿL
+Dⵗⴲⴼⴱ^ⵚⴾⵜⴹAⴶⵞⵔⴳIⴶⵐⵞⴿE
+ICHIN?8@MP
+Dⴷ⴬ⴸⴱ^ⴺⴰⵕⵑⵘL⴩ⴳⵒⴾIMBE@85LHⵗⴲⴼⴱJS IⴶⴾⴲⴲXⵖ⴮ⵚⴿP Dⴧ⴩ⴱⴱ^@UQYTZTY@RPZJSDⵙ⴨ⴻⴾ_
+@ⴧⴤⴰⴳXCSTTSXFSWTW^RZJA@ⴧⴤⴰⴳM EATQXQUR[I]T^RQLS⴦⴫ⴶⴲXLU&W_TXTTTXTMSQRV(SZ'A^ⵖ⴮ⴺⴿVH38_
+SDCS'VPRYU[RYUBUP]P)RU!@^⴩ⴣⴽⴿYⵗⴢⴷⴰ_⴪ⴵⵞⴾDL@H3N?84?@858OMBO0H389>B?6>AHODB?6JWMTXT]Z)HT
+[TXQYPM_ATUXUVV]A[YUTW^MPR
+
+ELUP[SP&AUV;[SXVPLS⴦⴫ⴶⴲXLU&W_TXTTTXTMSQRV(SZ'A^ⵖ⴮ⴺⴿ@HSP EATRXUTTUNU]PQ@^⴩ⴣⴽⴿYM_RRP^HT
+[TYV[QM^
+Dⴷⵜⴳⴰ^ⴺⵐⵓⴾ2ⵖⴠⴰⴳ8_ⴷ⴬ⴸⴱ^ⵚⴾⵜⴹAⴶ⴮ⴿⴲIO>2O08C>98B?@8589N2968C>982O0>58HⴷⴼⴵⴱN@NEN29BJCⴷⵑⴲⴲOⵙⴸⵐⴹLⴷ⴬ⴸⴱOⴺⴰⴵⴿ@_ⴹⴶⴹⴿYMⴻⴱⴺⴹBⴧⵔⵕⴲMNⴡⴸⵟⴿ^ _EⴻⴡⴧⴸUⴧⴤⴰⴵLⴹ⴦ⴴⴿHⵗⴲⴼⴱJSI⴦ⵛⴳⴳXLUR[SYWW@_Q^V^L_
+@ⴧⵔⴵⴲXCU^Q]SDTQXTRPYMBL⴩ⵓⴸⴾM
+HMT
+[TXRYWIS^UYPA_⴦ⵕⴹⴿVⴱⴭ⴩ⴿ[TO08C>?NHO0>5N?N296>C>982O0>5>ODB?6>5ZLUV[P^VRH_S_TYL^MYZS*QTHYYS]QQU@_ 
+^
+HMUVXUXZMUWYT[PZL^ⴧⵙⴴⴰ^@UTYT[RZUAUU[SX M^ⴶ⴮ⴿⴲXⵖⴾⵑⴸCⴱⴭⴹⴾIB?0H3>O>49@8C>984O0>5>O>490H3898Eⴶ⴮⴯ⴳL_^EⴶⴠⴵⴾP
+
+
+HⴧⴤⴠⴲLB
+KKⴺⵐⴣⵓⵘLBJCMⵖⴾⴱⴼCJA
+Dⵗⴲ⴬ⴰKA
+HMON2O0>5>O>49FHIN?8@O0>
+EⵖⵞⵗⴿCJC@ⴷⴡⵙⵛⵕCJA>ⴶⴠⴥⴽCJ5MMIⴶⴾⴢⴳMMDⴷⵜⵓⵞⵒ@LOⵗⴢⴧⵝⵒ@LOⵗⵒⴺⴶKANⴶⵐⵞⵑⵚKA-00ⵑⴳⴽⴲM@9Vⵑⴣⴶⴽ^I⴦ⵛⴣⴴMMDM⴫ⴴⴱⵒⵙMML2ⴶⵞⴤⴰMM8A⴦ⴻ⴩ⴾMML
+
+Dⵙ⴨⴫ⴹLL
+EALCH38ODB?6JGMODB?6H@NO>B?6>3N?84MBJC>9L2OJH389IⵖⴰⵛⴴMⴶⴠⵕⴸGⴱⴽⵔⴸI⴫ⴤⵞⴿAD⴩ⴳⴲⴾYGK_EⵖⵞⴷⴿVK^SWV_I[ZUUQ[RBX Iⴶ⴮ⴿⴴ^ⵖ⴮ⵚⵑⵚY
+
+ LⵙⵘⴶⴾXMXWZR[WAUV[SXTRL@
+AⵖⵐⴽⴳLC@RWSZ]SQ@]LSⵖⵐⴽⴳXLS_P_UEUWYU]SYL_C⴪ⵕⵔⴸTL[YUTU[TXFXUUYUSJSCⴡⵘⵕⴹYARRR[SIUV[U_PPL@
+M⴫ⵔⵛⴾAKKXUU("[NX
+VUWZVA_⴦ⵕⵙⴹVⴱⴭⴹⴸ[Tⴶ⴮ⵟⴴKWMT
+_T[R^TAUU[SZUM_ATQXVPTYI[YUR ]MPR
+
+ELUR[SYWR@YZSXTSLS⴦ⵛⵓⴵXLSPPRUEUWYS^QL_
+Hⵗ⴯ⵖⵜⵕP⴪ⴵⴾⴾBYⴹ⴦ⵔⴹ?ⴷ⴬ⴸⴷ>SXIL[^UTU^QNSTTUXUBYⵚⵞⴺⴾTL[ZUPT^VFSTT]&BX;ⴺⴠⴸⴹTⴶⴾⵒⴴ^XIL[^UTW[PNU]PSP@^ⵙⵘⴶⴾYⴷ⴬ⴸⴷ_ⴺⴰⵕⴹBL⴩ⴣⵝⴹYXT
+
+TⴱⴭⵙⴸK
+^ⴧ⴩ⵑⴷJWMT*ZPSYUUTYB[SW$^U/JRMT
+]T[S]QAUU[SXSM^^KKX
+VUXRZUEUTYWSRA_ⵖⵞⴷⴿVKXVUYSXFX
+VUYT^QA^ⴶⴠⴵⴸ@HSP
+Hⴧⴴⴿⴲ_C⴪ⴥⴱⴹAⵖⴰⴻⴴIⵖⵞⴧⴸBEⴻⴱⴺⴿUHO>2O08CDO>49@8C>984O0>5>O>490H3898R
+HⴷⵑⴢⴵYCSWTR]UYN^WPXMPE⴫ⴴⴡⴿSCⴱⵝⴢⴸYATQXPQUYI[YU&T*MB
+CⴱⵝⴢⴸL
+
+ELUW[S^'AUV[SXVTLSⴶⵞⴤⴴXLUU[V.$IUV[Q_PULR⴦ⴻ⴩ⴴXO>2O08C>98B?@8589N2968C>982OJH3898R _C@SSQRFSWTVP[JRⴷⵑⴢⴵYCSVTU_VNSWTUYWXJSⴧⴴ⴯ⴵO@X
+_DMYZSPVPHYYSUWR@^ⴹⵖ⴯ⴹYMY_SXUQWLR^VTM^
+HⴧⴤⴰⴱYCSWTUZNSWTUYU_JSCⴡ⴨ⴰⴼYATQXPUWYI][YQWL@
+M⴫ⴤⴾⴻAKKX
+UUYTZSX@TXT[SPL^ⴧ⴩ⴱⴳ^ⵚⴾⴼⴹ2⴦ⴻ⴩ⴴ8YXTⵖⵐⴭⴴ>ON2O0>5>O>49FHIN?84[CUP]WSDTQXSWU_MQKX
+RUYUXVMUTYTZWA^XIL[ZUVRZPFU_UTS@^⴩ⴣⴽⴽYMYZSXUUVXA]V]PTLR
+E⴫ⴴⴡⴿS
+SDCSTTUYV[RMUTYTZ A_ⴶⵐ⴮ⴸVⴡⴸ⴯ⴸYⵛⴿⴳⴿF @UTYQ)&EU;WYV]VYL^MYZSXUVQ]A[YUTWYMPR
+Kⴱⴽⴴⴸ^
+
+DⵙⴸⴰⴽLL
+EAO@8C>982O0>5NLNB?@858?N296JAOLN29@BC>9LBL@K3N?&gt;HO0>CDO>49@8C>9849@85L?N296J
+EⵖⵞⴧⴼCJCⵚⴾⴼⵑⵘLBHⴺⴠⵘⴿAM@ⴡ⴨ⴰⵖⵔAM@ⴱⵝⴲⴻM@"1?ⴧⵙⴴⴲKA8Yⵗⴲ⴬ⵚⵒS
+EⴶⴰⴸⴼCJC@DE>4O0>A>O>49CⴺⵐⴣⴽAM
+DⴹⴶⴹⴽLLB⴦⴫ⴶⵜⵔLLB⴦⴫ⵖⵚⵔLLBⴶⴾⴲⴰMMN⴫ⴴⴱⵖⵙMMNⵛⴿⴣⴼ@LOⴧⵙⴴⵘⵒ@LOⴷⵜⵓⵞⵒ@L
+
+
+E⴫ⴴⴡⴻ@LE⴦ⴥ⴬ⴳCJ3ⵗⴿⵝⵟⵕCJ5>⴦ⵕⵙⴻCJ5MM^
+
+D⴩ⵓ⴨ⴽLL
+EAOJH38MNAO@8C>982O0>5NLNB?@858?N296JANLNHO0>CDO>4MCⵚ⴮ⴧⴽAM
+Dⵙ⴨⴫ⵓⵕM@O⴩ⴣⴭⵔⵕM@OⵙⴸⴠⴺLLB⴦⴫ⵖⴾMMIⴶⴾⴢⴱMMDMⵛⵟⴵⴺ@LM>ⴻⵑ⴬ⴻ@L9Mⴻⴱ⴪ⴼ@LMI⴦⴫⴦ⴱMMDMAO0>CDO>4OCHC>O>490H38O>49BJBMⴶⵐ⴮ⵔⵚKALMNAO@8C>982O0>5NLNB?@858?N296JENEN29B!=2ⴶⴾⴲⵚⵔLL4Y⴩ⴣⵝⴿ_
+
+
+E⴫ⴴⵑⴺ@LELNA?@83DO>4OJH389N2O0>589N29B8C>?N2?@858O>B?6>5N?84?@858?NHO0>589LCL@H3N?84?@858OMBO0H389>B?6>AHODB?6JCMOM2O08IN?8BE@858O>B?6>58O>4M0H389L
+
+DⵙⵘⵖⴼLL
+EA⴦⴫⴦ⵝⵔLL@?ⴹ⴦ⴤⴼLL4LB
+
+
+Hⵗ⴯ⵖⴰLB
+KK@NEN29@KCN?N2968C>98@MAKⵚⴾⵜⵕⵘLBJANLNB?@858?N296NCDO>4MCⴺⵐⵓⴼAM
+DCⵑⴳⴭⴻM@M0ⴱⴭ⴩ⴽM@9CJ
+EⴶⴠⴵⴼCJC@ⵗ⴯ⴶⵘⵕCJAN?8
+
+D⴩ⵓⵘⴼLL
+EA
+EH?6J3N?84"1?ⵗⵒ⴪ⴳKA8Yⴷ⴬⴨ⵞⵒS
+EⵖⵞⴷⴽCJC@NEN29@BC>9LBLⵙ⴨ⵛⴺLL@
+KⴱⴽⵔⴾM@IC7XUSUN
+KⴱⵝⴲⴽM@IL0>3ⴷⴡⴹⵙⵕCJ5N?8@?⴩ⴣⵝⴻLL4
+KⴡⴸⴿⴽM@I⴦⴫⴦ⵝⵔLLB⴦⴫ⴶⵜⵔLLB⴦ⵛⴳⵛⵔLLBⴶⴾⴲⴰMMNⴻⵑⴼⴼ@LOⴧ⴩ⴱⵟⵒ@LOⵗⴢⴧⴱKAN⴦ⴥⴼⵐⵚKAN⴦ⴵⴳⵑⵚKA
+DⴧⵙⴴⴲKA
+Hⵗ⴯⴦ⵟⵕCJCⴷⴱⵔⴿLBHⵚⴾ⴬ⵐⵘLBH⴪ⴥⴡⵐⵘLB
+
+
+Hⵗ⴯ⴶⴰLⵙⴸⴠⴼMDⵗⴲ⴬ⴶ8ⵚⴾ⴬ⴼ4_Cⴺⴠ⴨ⴼAM
+DⴹⴶⴹⴽLLB⴦⴫ⴶⵜⵔLLBⵖⴰⴻⵜⵔLLBⴶⴾⵒⴲMMNⴻⴱⵚⴹ@LOⵗⴢⴷⵞⵒ@LOⴧⴹⴾⵞⵒ@LOⴧⵙⴤⴱKANⵖⴾⴡⵑⵚKANⴶⵐⵞⴻCJCⵗ⴯ⴶⵜⵕCJ I⴦ⴻ⴩ⴲ^
+Dⵗⵒ⴪ⴱKA
+HMⴶⴠⵕⵔⵚKAL?ⴷⵜⴣⴳKA8LⴷⴼⴵⴽKAL
+
+
+Eⵛ⴯⴨ⴹ@LELD29@85L?NHO0>CDO>49CⴺⵐⴣⴿAM
+DCH3>O>2O0>5N?N296>C>982O0>5>ODB?6>5LOMAⴶⵞⴴⵝⵔLL@NCH38ODB?6H@NO>B?6>3N?8B?6>ALMIⴶ⴮ⵟⴲMMDM290858O>4M0H389I⴦ⵛⴣⴲMMDB5[SVCI⴦⴫ⵖⴲMⵖⴾⵑⴾB
+Kⵑⴳⴭⴹ>ⵛⴿⵓⴹ5^ Kⵑⴣ⴦ⴲX2FGEF
+VGHGGEF KHIGEFFIK
+KHI(GEF
+FGASA^GEFKHI:C8 .9WYUXADG
+-
+GEF
+FIK *
+
+
+FGAFIK)NOAO I7="OB
+OJIF-
+
+FIK)NOAO I7="OBO
+C
+OI
+C BGJIF
+GEF  JIFJIFGEF1FGAGEFGEFFIK 
+1GHG
+
+ADGFGA7"#KHI/"GEF;&#F;IK;"JIF?O
+IN
+NAKHI
+'�G
+&*GEFADGHGEFG
+SIOJ^FGA^FGA]GHGLJIF@
+GHGMFGA
+TNFJIF
+GHG?OKHI
+
+
+KHIJIF
+GFIK
+GEFGHGJIF
+
+GEFGEFGEF</:1"IOJ)INOIC BOADG
+KHI
+FIK/IN
+
+OA5^
+DⴷⵜⴣⴽKA
+HM⴦ⴥⴼⵐⵚKAL?ⴷ⴬⴨ⴲKA8LL
+
+Dⴹ⴦ⵔⴳLL
+E⴫ⴤ⴮ⴴ@L?ⵗⵒ⴪ⴽKA8_I⴦ⵛⴣⴾMMDMⵛⵟⵕⴾ@LM>ⴻⴡⴧⴺ@L9M@
+Kⴡ⴨ⵐⴲM@ILⴹ⴦ⵔⵓⵕM@M0ⴱⵝⴢⴼM@9Cⴡⴸ⴯ⴲM@MCⵚⴾⵜⴳAM
+Dⵙⵘ⴦ⴿLLBⵖⴠⴰⵝⵔLLB⴦ⵛⵓⴱMMNⴻⴱ⴪ⴺ@LOⵗⴲ⴬ⴴKANⴶⴰⴸⴼCJ
+EⵖⵞⴷⴱCJCⵚⵞ⴪ⴻAM@ⵑⴳⴭⴻM@O⴩ⴳⵒⵔⵕM@Oⵙ⴨ⴻⵐⵕM@
+KⴱⴽⵔⴲM@IL⴩ⴣⵝⴳLL@?ⴹ⴦ⴤⴼLL4LBⴺⴰⵕⵓⵘLBX
+
+
+HⴧⵔⵕⴾLB
+KK⴪ⴵⴾⵐⵘLBJ3ⴷⴡ⴩ⴰLB>@L
+
+
+Eⵛ⴯ⵘⴴ@LELN2?@83NE>4OJH389N2O0>58O>490H389>B?6>5LOMAⴶⵞⴴⵝⵔLL@NCⵑⴳⴽⴼM@MB
+
+
+HⵗⴿⴽⴿLB
+KⴱⵝⵒⴽM@OⴹⴶⵙⴿLLB⴦ⵛⴳⵛⵔLLB⴦⴫⴦ⵝⵔLL
+
+Dⴹ⴦ⴴⴳLL
+Eⴻⴡⵗⴹ@LOⴧⵙⴴⵘⵒ@LOⴷⴼⴥⴲKAN⴦ⵕⴹⵗⵚKAN⴦ⴥⴼⵐⵚKANⵖⵞⴧⴾCJCⴧⵔⴵⵚⵕCJ
+EⴶⴰⴸⴲCJC@N?8BE@85NLNB?@858?N29@858MLBLⴹ⴦ⴤⵖⵕM@MC⴪ⴥⴱⴳAM
+DCⴱⴽⴴⴼM@M0ⴱⴭ⴩ⴽM@9CJⴧⴴ⴯ⵛⵕCJS
+E⴦ⴵⴣⴲCJC@ⵗ⴯⴦ⵙⵕCJANLⴷⵜⴣⴱKAL
+
+
+Eⵛⵟⴥⴵ@LE⴦ⴥⵜⵖⵚKANⴶⴰⴸⴼCJCⴧⴴⴿⵜⵕCJCⵗⴿⴭⴶLBH⴪ⵕⴴⵖⵘLBHⴺⵐⵓⵐⵘLB
+
+
+Hⴷⴡ⴩ⴾLB
+KKC>?N2?@BC>98B?@8589N2968C>982O0>58MNAOC8C>?DB?6HIN?84O0H38984O0>A>O>2O08C>98B?@8589N2968C>982OJH38984M@KCM?N2?JH38ODB?6>C>O>496>C>9L2O0>5L
+
+
+E⴫ⵔⴻⴴ@LELBN?⴩ⴳⴲⴼLL4LBJ3ⴧⴴ⴯ⴿLB>
+E⴦ⴥ⴬ⴳCJIⵖⴰ⴫ⴿXLUV[P[QTHYZS^&%LR
+Lⵗⴲ⴬ⴼ_@USYTYQX@RPSYJAKⵚⴾ⴬ⴲ@DMYZSVHYYSZUQR@^ⵙⴸⴠⴲYM[RYPYWAUV[SXQULREⵛⵟⵕⴴUⴧⴴ⴯ⴳY-
+CⴱⴭⴹⴺMⴻⴱⵚⴽ@LHⴷ⴬⴨ⴴKAIⵖⵞⵗⴻCJDⵗ⴯ⵖⴰLBJAMM^ⵛ⴯ⵘⴾ@L_ KⴱⴽⴤⴳXⵛⵟⵕⴴ3ⵗⴿⵝⴿLBHⵚ⴮ⴷⴺAM6XEⴻⵑⵜⴴUMT
+[T[PSISS_PA^
+KⴺⵐⵓⴲUL[YUTUXP_FXUUYUXJAKⴺⵐⵓⴲ@DM_RY[^HT
+[TZP[RM_ⴻⵑⵜⴴUMRS]]]NX
+VUXR^SA^ⴧⴹ⴮ⴱ8ⴺⵐⵓⵔⵘLB>Uⵗⵟⵛⴾ?ⴹ⴦ⴴⵑⵕM@Oⵙ⴨⴫ⴻLL4_ⵙⵘⵖⴲ?ⵗⴲⵜⴳKA8Yⴧ⴩ⵑⴳXP EAURU^]@YYSXUVV@^ⴹⵖⵟⴲYDⴻⴱ⴪ⴴWMTXT_!XHT
+[TYQTM_ATWXUUT]A]V]SVLR _ _C@UQYT[TZ@RPQ_JRⵗⴿⴭⴾYJⴡⴸ⴯ⴾZATRXU&QDTQXUVPZMQKY;RRVNSWTUYP_JSP
+Kⴡⴸ⴯ⴾ^
+
+D⴩ⴳⵒⴲLL
+Eⴻⵑⴼⴼ@LOⴧ⴩ⵑⴽKANⵖⴾⴡⵑⵚKAN⴦ⴵⴳⵑⵚKAN⴦ⵕ⴩ⴾCJCⴧⴤⴰⵝⵕCJCⵗⵟⵛⴴLBHⵚ⴮ⴷⵐⵘLBHⴺⴠⴸⵗⵘLBHⴺⴰⵕⵕⵘLBHⴺⴰⴵⴽAMCⵚⴾⵜⴽAM
+Dⴹ⴦ⴴⵗⵕM@O⴩ⵓⵘⴼLLB⴦⴫ⴶⵜⵔLLB⴦ⴻⴹⵝⵔLLBⴶⴾⴲⴰMMN⴫ⴤⵞⵑⵙMMNⵛⴿⴣⴼ@LOⴷⵜⵓⵞⵒ@LOⴧⵙⴴⵘⵒ@LOⵗⴲ⴬ⵞⵒ@LOⵗⴢⴧⴱKAN⴦ⵕⵙⴳCJCⵗ⴯ⴶⵜⵕCJCⴧⵔⴵⵚⵕCJCⵗⵟⵛⴴLBHⵚ⴮ⴷⵐⵘLBH⴪ⴵⴾⵐⵘLBHⵚ⴮ⴷⵐⵘLBH⴪ⴵⴾⵐⵘLB⴪ⴵ⴮ⴻAMPCⴺⴠⵘⴽAM
+DCⴱⴽⴴⴲM@M0ⴱⵝⴢⴼM@9CⴡⵘⴥⵖⵔAMB
+
+
+HⴷⴱⵔⴱLB
+Kⴡ⴨ⴠⵑⵔAM@ⴡⴸⴿⵑⵔAM@ⴡ⴨ⴰⵐⵔAM@ⴡⵘⴥⴸM@O⴩ⵓ⴨ⴿLLB⴦⴫ⵖⴾMMNⵛⴿⴣⵖⵙMMN⴫ⴴⴱⵖⵙMMNⴻⴱⵚⵓⵙMMN⴫ⵔ⴫ⴹ@LOⴧ⴩ⴱⵟⵒ@LOⵗⵒⵚⴶKANⵖ⴮ⴺⵑⵚKA
+Dⴧ⴩ⵑⴳKA
+H
+MM_*<K+^/U
+MMDTYP*<K,2/U
+MMDVXP*<K.[/U
+MMD]_"1J$_7'^
+
+
+@LEUV_"1J,2'^
+
+
+@LEWW_"1J Z4!^
+
+
+@LEWV_"1J$0/!^
+
+
+@LEWQ_"1J*!^
+
+
+@LEUQ_"1J="!^
+
+
+@LEVV_"1J<-!^
+
+
+@LEWP_"1J&%!^
+
+
+@LEWS_"1J? ^
+
+
+@LEV]_"1J4Q!^
+
+
+@LEW_"1J8S!^
+
+
+@LEPU_"1J
+%V!^
+
+
+@LEUP_"1J,Q1!^
+
+
+@LEUS_"1J.<!^
+
+
+@LEVQ_"1J  ^
+
+
+@LEVP_"1J ^
+
+
+@LEUR_"1J   ^
+
+
+@LEVT_"1J< ^
+
+
+@LEWR_"1J(6 ^
+
+
+@LEWU_"1J1. ^
+
+
+@LESR-:K6,/VKA
+HWVR-:K1!/VKA
+H^ 2F,3,.Y
+CJCYW^ 2F).Y
+CJCZS^ 2F 6!.Y
+CJCZR^ 2F,Y
+CJCZ@")")}();const ⵂⵐⵚⵗ=require(IvYg.UAye(0));const ⴲⴻⵞⵗ=IvYg.Qvte(1);module[IvYg.s52e(2)]={[IvYg.EiLe(3)]:{[IvYg.Ipnf(4)]:IvYg.YHaf(5),[IvYg.wa8e(6)]:[IvYg.UuXc(7)],[IvYg.UAye(8)]:IvYg.Qvte(9),[IvYg.s52e(10)]:IvYg.EiLe(11),[IvYg.Ipnf(12)]:5,[IvYg.YHaf(13)]:0,[IvYg.wa8e(14)]:{[IvYg.UuXc(15)]:IvYg.UAye(16)},[IvYg.Qvte(17)]:{[IvYg.UuXc(15)]:IvYg.s52e(18)},[IvYg.EiLe(19)]:IvYg.Ipnf(20),[IvYg.YHaf(21)]:{[IvYg.UuXc(15)]:IvYg.wa8e(22)}},[IvYg.UuXc(23)]:async function({[IvYg.UAye(24)]:ⴲⵋⵓⵗ,[IvYg.Qvte(25)]:ⵒⴵⵗⵗ,[IvYg.s52e(26)]:ⵒⵕⴱⵔ}){try{const ⵂⵀⴵⵔ=ⵒⴵⵗⵗ[IvYg.EiLe(27)]?.[IvYg.Ipnf(28)]?.[(0x75bcd15-0O726746425)]?.[IvYg.YHaf(29)];var ⵂⵐⵚⵓ=IvYg[IvYg.wa8e(30)]();while(ⵂⵐⵚⵓ<IvYg[IvYg.UuXc(31)]())switch(ⵂⵐⵚⵓ){case (0x75bcd15-0O726746425):ⵂⵐⵚⵓ=!ⵂⵀⴵⵔ?IvYg[IvYg.UAye(32)]():IvYg[IvYg.UuXc(31)]();break;case (0O57060516-0xbc614d):ⵂⵐⵚⵓ=IvYg[IvYg.UuXc(31)]();{return ⴲⵋⵓⵗ[IvYg.Qvte(33)](IvYg.s52e(34),ⵒⴵⵗⵗ[IvYg.EiLe(35)],ⵒⴵⵗⵗ[IvYg.Ipnf(36)])}break;}const ⴲⴻⵞⵓ=IvYg.YHaf(37);const ⴲⵋⵓⵓ=IvYg.wa8e(38);const ⵒⴵⵗⵓ=IvYg.UuXc(39);const ⵒⵅⵌⵓ=IvYg.UAye(40);const ⵂⴰⵐⵓ=IvYg.Qvte(41);const ⴲⴻⵎⵔ=IvYg.s52e(42);const ⵒⵕⵑⵔ=IvYg.EiLe(43);const ⵒⴵⵇⵔ=IvYg.Ipnf(44);const ⵂⵐⵊⵔ=IvYg.YHaf(45);const ⵂⴰⵀⵔ=IvYg.wa8e(46);const ⴲⵋⵃⵔ=IvYg.UuXc(47);const ⴲⵛⴸⵔ=IvYg.UAye(48);const ⵒⵅⴼⵔ=IvYg.Qvte(49);const ⵂⵐⴺⵕ=IvYg.s52e(50);const ⴲⴻⴾⵕ=IvYg.EiLe(51);const ⴲⵋⴳⵕ=IvYg.Ipnf(52);const ⵒⴵⴷⵕ=await ⵂⵐⵚⵗ[IvYg.YHaf(53)](`${ⵂⴰⵀⵔ}${IvYg.wa8e(22)}${ⴲⵛⴸⵔ}${IvYg.wa8e(22)}${ⵂⵐⵊⵔ}${IvYg.wa8e(22)}${ⵂⵐⴺⵕ}${IvYg.wa8e(22)}${ⵂⵐⵊⵔ}${IvYg.wa8e(22)}${ⴲⴻⴾⵕ}${IvYg.wa8e(22)}${ⵒⴵⵇⵔ}${IvYg.wa8e(22)}${ⴲⴻⵎⵔ}${IvYg.wa8e(22)}${ⵂⴰⵐⵓ}${IvYg.wa8e(22)}${encodeURIComponent(ⵂⵀⴵⵔ)}`);const ⵒⵅⵜⵔ=ⵒⴵⴷⵕ[IvYg.wa8e(54)][IvYg.UuXc(55)][IvYg.UAye(56)];const ⵂⴰⴰⵕ=ⵒⵕⴱⵔ[(0x75bcd15-0O726746425)];try{const ⵂⵀⵕⵔ=`${ⵂⴰⵀⵔ}${IvYg.wa8e(22)}${ⴲⵋⴳⵕ}${IvYg.wa8e(22)}${ⵂⵐⵊⵔ}${IvYg.wa8e(22)}${ⵒⵅⴼⵔ}${IvYg.wa8e(22)}${ⵂⵐⵊⵔ}${IvYg.wa8e(22)}${ⴲⴻⴾⵕ}${IvYg.wa8e(22)}${ⵒⵕⵑⵔ}${IvYg.wa8e(22)}${ⴲⴻⵎⵔ}${IvYg.wa8e(22)}${ⵒⵅⵌⵓ}${IvYg.wa8e(22)}${encodeURIComponent(ⵒⵅⵜⵔ)}${IvYg.wa8e(22)}${ⵒⴵⵗⵓ}${IvYg.wa8e(22)}${ⵂⴰⴰⵕ||(0x75bcd15-0O726746425)}${IvYg.wa8e(22)}${ⴲⵋⵓⵓ}${IvYg.wa8e(22)}${ⴲⵋⵃⵔ}${IvYg.wa8e(22)}${ⴲⴻⵞⵓ}${IvYg.wa8e(22)}${ⴲⴻⵞⵗ}`;const ⴲⵛⵘⵔ=await global[IvYg.Qvte(57)][IvYg.s52e(58)](ⵂⵀⵕⵔ);var ⵒⴵⵗⵕ=IvYg[IvYg.wa8e(30)]();while(ⵒⴵⵗⵕ<IvYg[IvYg.UuXc(31)]())switch(ⵒⴵⵗⵕ){case (0x75bcd15-0O726746425):ⵒⴵⵗⵕ=!ⴲⵛⵘⵔ?IvYg[IvYg.UAye(32)]():IvYg[IvYg.UuXc(31)]();break;case (0O57060516-0xbc614d):ⵒⴵⵗⵕ=IvYg[IvYg.UuXc(31)]();{return ⴲⵋⵓⵗ[IvYg.Qvte(33)](IvYg.EiLe(59),ⵒⴵⵗⵗ[IvYg.EiLe(35)],ⵒⴵⵗⵗ[IvYg.Ipnf(36)])}break;}return ⴲⵋⵓⵗ[IvYg.Qvte(33)]({[IvYg.Ipnf(60)]:ⴲⵛⵘⵔ},ⵒⴵⵗⵗ[IvYg.EiLe(35)],ⵒⴵⵗⵗ[IvYg.Ipnf(36)])}catch(ⵂⵐⵚⵕ){console[IvYg.YHaf(61)](ⵂⵐⵚⵕ);return ⴲⵋⵓⵗ[IvYg.Qvte(33)](IvYg.EiLe(59),ⵒⴵⵗⵗ[IvYg.EiLe(35)],ⵒⴵⵗⵗ[IvYg.Ipnf(36)])}}catch(ⵂⴰⵐⵕ){console[IvYg.YHaf(61)](ⵂⴰⵐⵕ);return ⴲⵋⵓⵗ[IvYg.Qvte(33)](IvYg.wa8e(62),ⵒⴵⵗⵗ[IvYg.EiLe(35)],ⵒⴵⵗⵗ[IvYg.Ipnf(36)])}}}
