f14dbe244f119dc62a9791e2f7116a2395dbed78
EASIRMAHI <136728953+EASIRMAHI@users.noreply.github.com>
2024-05-02 00:22:38 +0600

    Update Mahi.js

--- scripts/cmds/Mahi.js
+++ scripts/cmds/Mahi.js
@@ -2,7 +2,7 @@ module.exports = {
  config: {
  name: "mahi",
  version: "1.0",
- author: "mahi", // xue editz
+ author: "mahi", // hopeless 
  countDown: 5,
  role: 0,
  shortDescription: "no prefix",
@@ -14,7 +14,7 @@ module.exports = {
  if (event.body && event.body.toLowerCase() === "mahi") {
  return message.reply({
  body: "𝙳𝚘𝚗'𝚝 𝚌𝚊𝚕𝚕 𝚕𝚘𝚛𝚍 𝚊𝚒𝚣𝚎𝚗 𝚒𝚏 𝚑𝚎 𝚒𝚜𝚗'𝚝 𝚑𝚎𝚛𝚎 !!",
- attachment: await global.utils.getStreamFromURL("https://i.imgur.com/3drfzRj.mp4")
+ attachment: await global.utils.getStreamFromURL("https://i.imgur.com/Xl2vJSB.gif")
  });
  }
  }
