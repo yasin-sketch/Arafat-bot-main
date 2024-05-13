d2aa10626238b5560d435e2441cc5115c8db7722
Forkkorlechudbo <bristyk242@gmail.com>
2024-05-13 16:35:34 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -73,7 +73,7 @@ mentions:[{id:event.senderID, tag:nname}],
 			case "message_reaction":
 				onReaction();
         if(event.reaction == "❗"){
-  if(event.userID == "100083900196039"){
+  if(event.userID == "61553842932247"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
               });
@@ -83,7 +83,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
   }
   }
         if(event.reaction == "❌"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == '100072881080249','61553842932247'){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61553842932247"){
     message.unsend(event.messageID)
 }else{
     message.send(":)")
