955438828088dd013f39e9ab3f6052cecc3a0a3f
Forkkorlechudbo <bristyk242@gmail.com>
2024-06-10 01:12:44 +0600

    Update Info💋j.js

--- scripts/cmds/Info💋j.js
+++ scripts/cmds/Info💋j.js
@@ -1,110 +1,52 @@
+const axios = require('axios');
 const fs = require('fs');
-const moment = require('moment-timezone');
-const NepaliDate = require('nepali-date');
-const fast = require('fast-speedtest-api');
+const path = require('path');
 
 module.exports = {
   config: {
-    name: "info",
-    aliases: ['info', 'owner'],
-    version: "1.3",
-    author: "AceGun",
-    countDown: 5,
-    role: 0,
-    shortDescription: {
-      vi: "",
-      en: "Sends information about the bot and admin along with an image."
-    },
-    longDescription: {
-      vi: "",
-      en: "Sends information about the bot and admin along with an image."
-    },
-    category: "utility",
-    guide: {
-      en: "{pn}"
-    },
-    envConfig: {}
+    name: "owner",
+    version: "1.0",
+    author: "Mahi--",
+    description: "Displays the owner's information with a random GIF",
+    category: "info",
+    guide: "{p}owner"
   },
 
-  onStart: async function ({ message, api, event, usersData, threadsData }) {
-    const allUsers = await usersData.getAll();
-    const allThreads = await threadsData.getAll();
-    const speedTest = new fast({
-        token: "YXNkZmFzZGxmbnNkYWZoYXNkZmhrYWxm",
-        verbose: false,
-        timeout: 10000,
-        https: true,
-        urlCount: 5,
-        bufferSize: 8,
-        unit: fast.UNITS.Mbps
-      });
-    const result = await speedTest.getSpeed();
-    const botName = global.GoatBot.config.nickNameBot;
-    const botPrefix = global.GoatBot.config.prefix;
-    const authorName = global.GoatBot.config.authorName;
-    const authorFB = global.GoatBot.config.authorFB;
-    const authorInsta = "https://www.instagram.com/editz_of_easir";
-    const authorEmail = global.GoatBot.config.authorEmail;
-    const authorfav = "anime, programming, musterbating ";
-    const status = "just waiting for a perfect life partner";
-    const timeStart = Date.now();
+  onStart: async function ({ api, event }) {
+    const ownerInfo = `
+      👑 Owner Information 👑
+      Name: Mahi
+      Role: Bot Developer
+      Contact: hopelessmahi@gmail.com
+      Location: Rajshahi, Bangladesh
+      About: Passionate about coding and developing chatbots.
+    `;
 
-    const urls = JSON.parse(fs.readFileSync('scripts/cmds/xnil/info.json'));
-    const link = urls[Math.floor(Math.random() * urls.length)];
+    const gifs = [
+      "https://i.ibb.co/sWq2V1m/image.gif",
+      "https://i.ibb.co/WG56PNV/image.gif",
+      "https://i.ibb.co/hVX2NDz/image.gif",
+      "https://i.ibb.co/KrY1DF8/image.gif",
+      "https://i.ibb.co/7bFCMxH/image.gif",
+      "https://i.ibb.co/5cBmXj2/image.gif",
+      "https://i.ibb.co/7Qd7bLt/image.gif"
+    ];
 
-    // Get current date and time in Asia/Kathmandu timezone
-    const now = moment().tz('Asia/Dhaka');
-    const date = now.format('MMMM Do YYYY');
-    const time = now.format('h:mm:ss A');
+    const randomGifUrl = gifs[Math.floor(Math.random() * gifs.length)];
 
-    // Calculate bot uptime
-    const uptime = process.uptime();
-    const uptimeString = formatUptime(uptime);
+    try {
+      const response = await axios.get(randomGifUrl, { responseType: 'stream' });
+      const attachment = response.data;
 
-    const ping = Date.now() - timeStart;
+      const messageData = {
+        body: ownerInfo,
+        attachment
+      };
 
-    const replyMessage = `===「 Bot & Owner Info 」===
-❏ Bot Name: ${botName}
-❏ Bot Prefix: ${botPrefix}
-❏ Author Name: ${authorName}
-❏ FB: ${authorFB}
-❏ Instagram: ${authorInsta}
-❏ Author Email: ${authorEmail}
-❏ Author favorite: ${authorfav}
-❏ Status: ${status}
-❏ Date: ${date}
-❏ Total Threads: ${allThreads.length}
-❏ Total Users: ${allUsers.length}
-❏ Time: ${time}
-❏ Bot Running: ${uptimeString}
-❏ Bot's Speed: ${result} MBPS
-=====================`;
-
-    const attachment = await global.utils.getStreamFromURL(link);
-    message.reply({
-      body: replyMessage,
-      attachment
-    });
-  },
-
-  onChat: async function({ event, message, getLang }) {
-    if (event.body && event.body.toLowerCase() === "info") {
-      await this.onStart({ message });
+      api.sendMessage(messageData, event.threadID);
+    } catch (error) {
+      console.error("Error fetching the GIF:", error);
+      api.sendMessage(ownerInfo, event.threadID);
     }
   }
 };
-
-function formatUptime(uptime) {
-  const seconds = Math.floor(uptime % 60);
-  const minutes = Math.floor((uptime / 60) % 60);
-  const hours = Math.floor((uptime / (60 * 60)) % 24);
-  const days = Math.floor(uptime / (60 * 60 * 24));
-
-  const uptimeString = [];
-  if (days > 0) uptimeString.push(`${days}d`);
-  if (hours > 0) uptimeString.push(`${hours}h`);
-  if (minutes > 0) uptimeString.push(`${minutes}min`);
-  if (seconds > 0) uptimeString.push(`${seconds}sec`);
-
-  return uptimeString.join(" ");
-      }
