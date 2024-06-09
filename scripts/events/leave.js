f7268171d4b14e099d95ba940bacbd84dc57f583
Forkkorlechudbo <bristyk242@gmail.com>
2024-06-10 01:28:25 +0600

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -25,7 +25,7 @@ module.exports = {
 			session4: "evening",
 			leaveType1: "left",
 			leaveType2: "was kicked from",
-			defaultLeaveMessage: "{userNameTag} {type} the group"
+			defaultLeaveMessage: "{userName} {type} the group"
 		}
 	},
 
