77dd0a7d26d668143543e4210b2c6a17bfda149c
Forkkorlechudbo <bristyk242@gmail.com>
2024-06-28 19:48:24 +0600

    Update prodia.js

--- scripts/cmds/prodia.js
+++ scripts/cmds/prodia.js
@@ -1,51 +1,103 @@
-const fs = require("fs");
-const axios = require("axios");
-
+const axios = require('axios');
 module.exports = {
   config: {
-    name: "prodia",
-    aliases: [],
-    author: "kshitiz & arjhil",
-    version: "2.0",
-    cooldowns: 5,
+    name: 'prodia',
+    version: '1.0',
+    author: 'rehat--',
+    countDown: 0,
     role: 0,
-    shortDescription: {
-      en: ""
-    },
     longDescription: {
-      en: "generate an image"
+      en: 'Text to Image'
     },
-    category: "image",
+    category: 'ai',
     guide: {
-      en: "[prompt | model]"
+      en: `1 | 3Guofeng3
+2 | Absolutereality_V16
+3 | Absolutereality_v181
+4 | AmIReal_V41
+5 | Analog-diffusion-1.0
+6 | Anythingv3_0-pruned
+7 | Anything-v4.5-pruned
+8 | AnythingV5_PrtRE
+9 | AOM3A3_orangemixs
+10 | Blazing_drive_v10g
+11 | Breakdomain_I2428
+12 | Breakdomain_M2150
+13 | CetusMix_Version35
+14 | ChildrensStories_v13D
+15 | ChildrensStories_v1SemiReal
+16 | ChildrensStories_v1ToonAnime
+17 | Counterfeit_v30
+18 | Cuteyukimixadorable_midchapter3
+19 | Cyberrealistic_v33
+20 | Dalcefo_v4
+21 | Deliberate_v2
+22 | Deliberate_v3
+23 | Dreamlike-anime-1.0
+24 | Dreamlike-diffusion-1.0
+25 | Dreamlike-photoreal-2.0
+26 | Dreamshaper_6BakedVae
+27 | Dreamshaper_7
+28 | Dreamshaper_8
+29 | Edgeofrealism_eorV20
+30 | Eimisanimediffusion_v1
+31 | Elldreths-vivid-mix
+32 | Epicphotogasm_xplusplus
+33 | Epicrealism_naturalsinrc1vae
+34 | Epicrealism_pureevolutionv3
+35 | Icantbelieveitsnotphotography_seco
+36 | Indigofurrymix_v75hybrid
+37 | Juggernaut_aftermath
+38 | Lofi_v4
+39 | Lyriel_v16
+40 | Majicmixrealistic_v4
+41 | Mechamix_v10
+42 | Meinamix_meinav9
+43 | Meinamix_meinav11
+44 | Neverendingdream_v122
+45 | Openjourney_v4
+46 | Pastelmixstylizedanime_pruned_fp16
+47 | Portraitplus_v1.0
+48 | Protogenx34
+49 | Realistic_vision_v1.4-pruned-fp16
+50 | Realistic_vision_v2.0
+51 | Realistic_vision_v4.0
+52 | Realistic_vision_v5.0
+53 | Redshift_diffusion-v10
+54 | Revanimated_v122
+55 | Rundiffusionfx25d_v10
+56 | Rundiffusionfx_v10
+57 | Sdv1_4
+58 | V1-5-pruned-emaonly
+59 | V1-5-inpainting
+60 | Shoninsbeautiful_v10
+61 | Theallys-mix-ii-churned
+62 | Timeless-1.0
+63 | Toonyou_beta6`
     }
   },
-  onStart: async function ({ api, event, args }) {
+
+  onStart: async function ({ message, args, event, api }) {
     try {
-      let text = args.join(" ");
-
-      const prompt = text.substr(0, text.indexOf(' | '));
-      const model = text.split(" | ").pop();
-
-      if (!prompt || !model) {
-        return api.sendMessage('Please provide a prompt and a model.', event.threadID);
+      const info = args.join(' ');
+      const [prompt, model] = info.split('|').map(item => item.trim());
+      const text = args.join (" ");
+      if (!text) {
+        return message.reply("Add something baka.");
       }
+      const modelParam = model || '35';
+      const apiUrl = `https://rehatdesu.xyz/api/imagine/prodia?prompt=${prompt}&model=${modelParam}&apikey=rehat666`;
 
-      const encodedPrompt = encodeURIComponent(prompt);
+      await message.reply('Please Wait...⏳');
+      const form = {
+      };
+      form.attachment = [];
+      form.attachment[0] = await global.utils.getStreamFromURL(apiUrl);
 
-      const loadingMessage = 'Generating image. Please wait...';
-      api.sendMessage(loadingMessage, event.threadID, event.messageID);
-
-      const providedURL = `https://arjhil-prodia-api.arjhilbard.repl.co/sdxl/generate?prompt=${encodedPrompt}&model=${model}`;
-
-      const response = await axios.get(providedURL, { responseType: 'stream' });
-
-      api.sendMessage({
-        attachment: response.data,
-      }, event.threadID, event.messageID);
+      message.reply(form);
     } catch (error) {
       console.error(error);
-      api.sendMessage('An error occurred while processing the sdxl command.', event.threadID);
+      await message.reply('❌ | Sorry, API Have Skill Issue');
     }
   }
 };
